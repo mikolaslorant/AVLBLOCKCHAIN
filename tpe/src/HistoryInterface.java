2a63150ef31025133508a27f21a17a7ae7446283
mikolaslorant <lmikolas@itba.edu.ar>
2017-10-13 20:32:05 -0300

    Delete HistoryInterface.java

--- tpe/src/HistoryInterface.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sswinnen on 21/09/17.
- */
-public interface HistoryInterface<T> {
-    public void clean(int distance);
-    public int getCount();
-    public void add(int index, AVLTree<T> tree);
-    public boolean remove(int index);
-}
